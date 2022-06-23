44504002b32231f24a891abfb31e25144c60bc17
smectaz <90684643+smectaz@users.noreply.github.com>
2022-06-23 04:33:08 +0200

    cours condition

---
+++ src/components/ShoppingList.js
@@ -0,0 +1,30 @@
+import { plantList } from "../datas/plantList";
+
+function ShoppingList() {
+	const categories = plantList.reduce(
+		(acc, plant) =>
+			acc.includes(plant.category) ? acc : acc.concat(plant.category),
+		[],
+	);
+	return (
+		<div>
+			<ul>
+				{categories.map((cat) => (
+					<li key={cat}>{cat}</li>
+				))}
+			</ul>
+			<ul>
+				{plantList.map((plant) => (
+					<li key={plant.id}>
+						{plant.name}
+						{plant.isBestSale && plant.category === "classique" && (
+							<span>🔥</span>
+						)}
+					</li>
+				))}
+			</ul>
+		</div>
+	);
+}
+
+export default ShoppingList;
