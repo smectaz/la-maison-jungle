95d9e210e697f23650ef8208fb06c986633f8073
smectaz <90684643+smectaz@users.noreply.github.com>
2022-06-23 02:42:42 +0200

    update style banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,5 +1,10 @@
+import "../styles/Banner.css";
 function Banner() {
-	return <h1>La maison jungle</h1>;
+	return (
+		<div className="lmj-banner">
+			<h1>La maison jungle</h1>
+		</div>
+	);
 }
 
 export default Banner;
