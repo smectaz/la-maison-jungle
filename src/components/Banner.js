6d0a381246853bd64e1612522ec9ceb4b1c560dc
smectaz <90684643+smectaz@users.noreply.github.com>
2022-06-23 03:07:54 +0200

    exercice react style

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,8 +1,12 @@
 import "../styles/Banner.css";
+import logo from "../assets/logo.png";
+
 function Banner() {
+	const title = "La maison jungle";
 	return (
 		<div className="lmj-banner">
-			<h1>La maison jungle</h1>
+			<img src={logo} alt="la maison jungle" className="lmj-logo" />
+			<h1 className="lmj-title">{title}</h1>
 		</div>
 	);
 }
