8b12aaab8d8f4a65960044b4e627fae705dde704
smectaz <90684643+smectaz@users.noreply.github.com>
2022-06-24 02:43:26 +0200

    exercice form et fin de cours form

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,7 @@ import Banner from "./Banner";
 import logo from "../assets/logo.png"
 //import Cart from "./Cart";
 import ShoppingList from "./ShoppingList";
-
-function handleSubmit(e) {
-    e.preventDefault()
-    alert(e.target['my_input'].value)
-}
+import Footer from "./Footer";
 
 function App() {
 	return (
@@ -15,14 +11,12 @@ function App() {
 				<img src={logo} alt="La maison jungle" className="lmj-logo" />
 				<h1 className="lmj-title">La maison jungle</h1>
 			<Banner />
+
 			{/* <Cart /> */}
+
 			<ShoppingList />
-		
-		
-		<form onSubmit={handleSubmit}>
-		<input type='text' name='my_input' defaultValue='Tapez votre texte' />
-		<button type='submit'>Entrer</button>
-	</form>
+			
+			<Footer />
 	
 	</div>
 	)
--- src/components/CareScale.js
+++ src/components/CareScale.js
@@ -1,11 +1,19 @@
 import Sun from "../assets/sun.svg"
 import Water from "../assets/water.svg"
 
-function CareScale({scaleValue, careType}) {
-	//const {scaleValue, careType} = props
+//const {scaleValue, careType} = props
 	// On évite de multiplier les déclarations qui sans cette syntaxe auraient été :
 	// const scaleValue = props.scaleValue et
 	// const careType = props.careType
+
+	const quantityLabel = {
+		1: 'peu',
+		2: 'modérément',
+		3: 'beaucoup'
+	}
+
+function CareScale({scaleValue, careType}) {
+	
 	const range = [1, 2, 3]
 	const scaleType = 
 		careType === 'light' ? (
@@ -16,7 +24,15 @@ function CareScale({scaleValue, careType}) {
 	
 	
 	return (
-		<div>
+		<div 
+		onClick={() =>
+			alert(
+				`Cette plante requiert ${quantityLabel[scaleValue]} ${
+					careType === 'light' ? 'de lumière' : "d'arrosage"
+				}`
+			)
+		}
+		>
             {range.map((rangeElem) => 
 				scaleValue >= rangeElem ? (
 					<span key={rangeElem.toString()}>{scaleType}</span>
